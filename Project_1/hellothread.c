6b5a0e49a9fe0ca5bef717c1c1c79632386ef175
Ryan Earl <ryanleoearl@gmail.com>
2023-04-03 13:26:31 -0700

    hellothread file outputting properly

---
+++ Project_1/hellothread.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <pthread.h>
+
+void *run(void *arg)
+{
+	char *string_to_print = arg;
+	int i;
+	for (i=0; i<5; i++){
+		printf("%s: %d\n", string_to_print, i);
+	}
+	return NULL;
+}
+
+int main(void)
+{
+	pthread_t t1, t2;
+	// int x = 12;
+	printf("%s\n", "Launching threads");
+	pthread_create(&t1, NULL, run, "thread 1");
+	pthread_create(&t2, NULL, run, "thread 2");
+	printf("%s\n", "Threads complete!");
+
+}
