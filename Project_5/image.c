262cb7302e6b14c93b66f8fb10ed21acb5641308
Ryan Earl <ryanleoearl@gmail.com>
2023-05-08 15:16:00 -0700

    Converted to tabs

--- Project_5/image.c
+++ Project_5/image.c
@@ -5,14 +5,14 @@
 int image_fd;
 
 int image_open(char *filename, int truncate) {
-    int flags = O_RDWR | O_CREAT;
-    if (truncate) {
-        flags |= O_TRUNC;
-    }
-    image_fd = open(filename, flags, 0600);
-    return image_fd;
+	int flags = O_RDWR | O_CREAT;
+	if (truncate) {
+		flags |= O_TRUNC;
+	}
+	image_fd = open(filename, flags, 0600);
+	return image_fd;
 }
 
 int image_close() {
-    return close(image_fd);
+	return close(image_fd);
 }
