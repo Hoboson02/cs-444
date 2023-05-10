e898ca9b3369d287c4c8289e8e1d5aa3651682fb
Ryan Earl <ryanleoearl@gmail.com>
2023-05-09 22:49:22 -0700

    inode completed based on assignment instructions

--- Project_5/inode.c
+++ Project_5/inode.c
@@ -4,5 +4,12 @@
 
 
 int ialloc(void) {
-
+  unsigned char inode_map[BLOCK_SIZE];
+  bread(FREE_INODE_BLOCK_NUM, inode_map);
+  int low_free_bit = find_free(inode_map);
+  if(low_free_bit != -1) {
+    set_free(inode_map, low_free_bit, 1);
+  }
+  bwrite(FREE_INODE_BLOCK_NUM ,inode_map);
+  return low_free_bit;
 }
