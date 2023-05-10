438218838a2242edbcfa12b9e8e3b2d98300f25b
Ryan Earl <ryanleoearl@gmail.com>
2023-05-09 21:49:14 -0700

    Added tests for image and block

--- Project_5/simfs_test.c
+++ Project_5/simfs_test.c
@@ -9,6 +9,27 @@
 #include "mkfs.h"
 #include "ctest.h"
 
-int main(void) {
 
+void test_image() {
+  image_open("test_file.img", 0);
+  image_close();
+}
+
+void test_blockb() {
+  unsigned char block[BLOCK_SIZE];
+  unsigned char block2[BLOCK_SIZE];
+  memset(block, 'h', BLOCK_SIZE);
+  bwrite(12, block);
+  bread(12, block2);
+  if (memcmp(block, block2, BLOCK_SIZE) !=0) {
+    printf("Test Failed: The blocks produce differing results");
+  }
+  else {
+    printf("Test Passed: Both blocks produce the same result");
+  }
+}
+
+int main(void) {
+  test_image();
+  test_blockb();
 }
