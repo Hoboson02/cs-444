b14e467ea4434405c570475cfa537a10fa9f4853
Ryan Earl <ryanleoearl@gmail.com>
2023-05-09 20:50:44 -0700

    Added header includes to test file and took ctest code from beej repo and added it to ctest header file

---
+++ Project_5/simfs_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "image.h"
+#include "block.h"
+#include "free.h"
+#include "inode.h"
+#include "mkfs.h"
+#include "ctest.h"
+
+int main(void) {
+
+}
