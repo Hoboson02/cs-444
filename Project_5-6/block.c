b934bb532728320c02281894a31197bf11d7071e
Ryan Earl <ryanleoearl@gmail.com>
2023-05-21 20:09:36 -0700

    Fixed improper indentation in block

--- Project_5-6/block.c
+++ Project_5-6/block.c
@@ -4,16 +4,16 @@
 #include "free.h"
 
 unsigned char *bread(int block_num, unsigned char *block) {
-    int offset = block_num * BLOCK_SIZE;
-    lseek(image_fd, offset, SEEK_SET);
-    read(image_fd, block, BLOCK_SIZE);
-    return block;
+  int offset = block_num * BLOCK_SIZE;
+  lseek(image_fd, offset, SEEK_SET);
+  read(image_fd, block, BLOCK_SIZE);
+  return block;
 }
 
 void bwrite(int block_num, unsigned char *block) {
-    int offset = block_num * BLOCK_SIZE;
-    lseek(image_fd, offset, SEEK_SET);
-    write(image_fd, block, BLOCK_SIZE);
+  int offset = block_num * BLOCK_SIZE;
+  lseek(image_fd, offset, SEEK_SET);
+  write(image_fd, block, BLOCK_SIZE);
 }
 
 int alloc(void) {
