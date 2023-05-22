91199f5e58281f588d7471a0885a66d6947d9dbc
Ryan Earl <ryanleoearl@gmail.com>
2023-05-21 20:03:57 -0700

    Added additinoal variable definitions to inode header file

--- Project_5-6/inode.h
+++ Project_5-6/inode.h
@@ -1,6 +1,10 @@
 #ifndef INODE_H
-#define INODE_H
 
+#define INODE_H
+#define BLOCK_SIZE 4096
+#define INODE_SIZE 64
+#define INODE_FIRST_BLOCK 3
+#define INODES_PER_BLOCK (BLOCK_SIZE / INODE_SIZE)
 #define FREE_INODE_BLOCK_NUM 1
 
 int ialloc(void);
